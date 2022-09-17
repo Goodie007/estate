f880ef3a4d4b460c29dad24926771110870b134d
Aruoture Goodness <aruoturegoodness@gmail.com>
2022-09-17 13:54:11 +0100

    build up

--- components/UI/ProfilePage/index.tsx
+++ components/UI/ProfilePage/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import { View, Text, Image }  from 'react-native'
+import React, {useState, useEffect} from 'react'
+import { View, Text, Image, Button }  from 'react-native'
 import { EvilIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
+
+// interface ImageProps {
+//     uri: string;
+// }
 
 
 export default function ProfilePage() {
+    const [image, setImage] = useState(null);
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.cancelled) {
+           setImage(result.uri);
+        }
+      };
+
     return (
         <View
             style={{
@@ -13,14 +36,14 @@ export default function ProfilePage() {
             }}
         >
           
-            <Image
+             <Image
                 source={{ uri: 'https://images.pexels.com/photos/13195312/pexels-photo-13195312.jpeg'}}
                 style={{
                     height: 120,
                     width: 120,
                     borderRadius: 120 / 2,
                 }}
-             />
+             /> 
              <View
                 style={{
                     position: 'absolute',
@@ -35,16 +58,23 @@ export default function ProfilePage() {
                     opacity: 0.6,
                 }}
              >
-                <EvilIcons name="camera" size={24} color="#fff" />
+                <EvilIcons name="camera" size={24} color="#fff" onPress={pickImage} />
              </View>
              <Text
                 style={{
                     paddingTop: 20,
                     fontSize: 16,
                     fontWeight: '500',
-                    color: '#545454'
+                    color: '#545454',
+                    
                 }}
              >Mark Kwain</Text>
+
+            {/* <View style={{  alignItems: 'center', justifyContent: 'center', backgroundColor: "red" }}>
+                <Button title="Pick an image from camera roll" onPress={pickImage} />
+                <Text>Hwllo</Text>
+                {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+            </View> */}
         </View>
     )
 }
