4965b9d896a714d2a90da7a0bdf401976117ff7f
Aruoture Goodness <aruoturegoodness@gmail.com>
2022-09-19 14:21:24 +0100

    build up

--- components/UI/Icons.tsx
+++ components/UI/Icons.tsx
@@ -11,7 +11,7 @@ type SingleIcon = Omit<IconProps, "type">;
 const List = ({ color}: SingleIcon) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-        <path fill="#fff" d="M0 0L0 512L512 512L512 0L0 0z" color={ color} />
+            <path fill="#fff" d="M0 0L0 512L512 512L512 0L0 0z" color={ color} />
         </svg>
     )
 }
